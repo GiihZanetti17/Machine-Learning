84d9b29da5b87da9c57315451084f2fb901d29c4
Giovanna Zanetti Borges <94799644+GiihZanetti17@users.noreply.github.com>
2024-03-05 10:17:17 -0300

    Update Classificação com SKLearn.py

--- Study/Classificação com SKLearn.py
+++ Study/Classificação com SKLearn.py
@@ -17,29 +17,32 @@ cachorro3 = [1, 1, 1]
 
 # 1 => porco, 0 => cachorro
 
-dados = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3]
-classe = [1,1,1,0,0,0]
+treino_x = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3]
+treino_y = [1,1,1,0,0,0] #labels
 
 from sklearn.svm import LinearSVC 
 
 model = LinearSVC()
-model.fit (dados, classe)
+model.fit (treino_x, treino_y)
 
 animal_misterioso =[1,1,1]
 model.predict ([animal_misterioso])
-
 
 misterio1 = [1,1,1]
 misterio2 = [1,1,0]
 misterio3 = [0,1,1]
 
-teste = [misterio1, misterio2, misterio3]
-previsoes = model.predict (teste)
+teste_x = [misterio1, misterio2, misterio3]
+teste_y = [0,1,1]
 
-teste_classe = [0,1,1]
+previsoes = model.predict (teste_x)
 
-
-corretos = (previsoes == teste_classe).sum()
-total = len(teste)
+corretos = (previsoes == teste_y).sum()
+total = len(teste_x)
 taxa_de_acerto = corretos/total
 print ("Taxa de acerto: ", taxa_de_acerto *100)
+
+from sklearn.metrics import accuracy_score
+
+taxa_de_acerto = accuracy_score(teste_y, previsoes)
+print("Taxa de acerto", taxa_de_acerto *100)
