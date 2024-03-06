aff779dc3d7c49d77665d702f3af6ebd263d6c26
Giovanna Zanetti Borges <94799644+GiihZanetti17@users.noreply.github.com>
2024-03-06 11:10:37 -0300

    Update Aula03.py

--- Study/Classificação com SKLearn/Aula03.py
+++ Study/Classificação com SKLearn/Aula03.py
@@ -1,1 +1,13 @@
+import pandas as pd
+uri = "https://gist.githubusercontent.com/guilhermesilveira/1b7d5475863c15f484ac495bd70975cf/raw/16aff7a0aee67e7c100a2a48b676a2d2d142f646/projects.csv"
 
+dados = pd.read_csv(uri)
+dados.head()
+
+a_renomear = {
+    'expected_hours' : 'horas_esperadas',
+    'price' : 'preco',
+    'unfinished' : 'nao_finalizado'
+}
+
+dados.rename(columns = a_renomear)
